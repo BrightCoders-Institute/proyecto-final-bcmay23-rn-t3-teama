d721b361c0ea2ce3c9a4c85fa0e0528d29aa8bc8
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-08-02 13:35:59 -0600

    scrollable screen when keyboard is open

--- src/screens/LoginScreen/LoginScreen.tsx
+++ src/screens/LoginScreen/LoginScreen.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   SafeAreaView,
   View,
   useWindowDimensions,
   Image,
+  KeyboardAvoidingView,
+  ScrollView,
 } from 'react-native';
-import {styles} from './styles';
+import { styles } from './styles';
 import logo from '../../assets/img/logo.png';
 import fruits from '../../assets/img/fruits.png';
 import { ButtonPrimary } from '../../components/ButtonPrimary/ButtonPrimary';
@@ -34,6 +36,7 @@ const LoginScreen = () => {
     setErrorPwText,
     setErrorClientKeyText,
   } = useFieldValidation();
+  const [isDisabledLogInBtn, setIsDisabledLogInBtn] = useState(true);
   const {height} = useWindowDimensions();
 
   const handleInputChangeAndValidation = (name: string, value: any) => {
@@ -42,50 +45,63 @@ const LoginScreen = () => {
     handleFieldValidation(name, value);
   };
 
+  const handleDisabledLoginButton = () => {
+    console.log('Loggin in ...');
+    if (
+      isNameValid &&
+      name.trim() !== '' &&
+      isPasswordValid &&
+      password.trim() !== '' &&
+      isClientKeyValid &&
+      clientKey.trim() !== ''
+    ) {
+      setIsDisabledLogInBtn(false);
+      return;
+    }
+    setIsDisabledLogInBtn(true);
+  };
+
   return (
     <SafeAreaView>
-      <View style={{height: height * 0.5}}>
-        <Image
-          source={logo}
-          style={[styles.logo, {height: height * 0.2}]}
-          resizeMode="contain"
-        />
-        <Image
-          source={fruits}
-          style={[styles.logo, {height: height * 0.28}]}
-          resizeMode="contain"
-        />
-      </View>
+      <KeyboardAvoidingView behavior="height">
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+          <View style={{ height: height * 0.5 }}>
+            <Image source={logo} style={[styles.logo, { height: height * 0.2 }]} resizeMode="contain" />
+            <Image source={fruits} style={[styles.logo, { height: height * 0.28 }]} resizeMode="contain" />
+          </View>
 
-      <View style={{height: height * 0.3, flexDirection: 'column', justifyContent: 'space-between'}}>
-        <TextFieldForm
-          placeholder="Name"
-          inputValue={name}
-          onInputChange={value => handleInputChangeAndValidation('name', value)}
-          invalidText={errorNameText}
-          isInputValid={isNameValid}
-          setInputValid={setIsNameValid}
-        />
-        <TextFieldForm
-          placeholder="Password"
-          inputValue={password}
-          onInputChange={value => handleInputChangeAndValidation('password', value)}
-          invalidText={errorPwText}
-          isInputValid={isPasswordValid}
-          setInputValid={setIsPasswordValid}
-        />
-        <TextFieldForm
-          placeholder="Client Key"
-          inputValue={clientKey}
-          onInputChange={value => handleInputChangeAndValidation('clientKey', value)}
-          invalidText={errorClientKeyText}
-          isInputValid={isClientKeyValid}
-          setInputValid={setIsClientKeyValid}
-        />
-      </View>
-      <View style={{height: height * 0.2, justifyContent: 'center'}}>
-        <ButtonPrimary title='LogIn' onPress={() => console.log('Loggin in ...')} />
-      </View>
+          <View style={{ height: height * 0.3, flexDirection: 'column', justifyContent: 'space-between' }}>
+            <TextFieldForm
+              placeholder="Name"
+              inputValue={name}
+              onInputChange={(value) => handleInputChangeAndValidation('name', value)}
+              invalidText={errorNameText}
+              isInputValid={isNameValid}
+              setInputValid={setIsNameValid}
+            />
+            <TextFieldForm
+              placeholder="Password"
+              inputValue={password}
+              onInputChange={(value) => handleInputChangeAndValidation('password', value)}
+              invalidText={errorPwText}
+              isInputValid={isPasswordValid}
+              setInputValid={setIsPasswordValid}
+            />
+            <TextFieldForm
+              placeholder="Client Key"
+              inputValue={clientKey}
+              onInputChange={(value) => handleInputChangeAndValidation('clientKey', value)}
+              invalidText={errorClientKeyText}
+              isInputValid={isClientKeyValid}
+              setInputValid={setIsClientKeyValid}
+            />
+          </View>
+
+          <View style={{ height: height * 0.2, justifyContent: 'center' }}>
+            <ButtonPrimary title="LogIn" isValid={false} onPress={handleDisabledLoginButton} />
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
