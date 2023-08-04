47577dfbd66b9b2e83c6afb0f9fe492259e2c6dc
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-08-03 19:10:46 -0600

    get data from Firebase
    Co-authored-by: Montserrat Aguilar <aguilarvalle.montse@gmail.com>
    Co-authored-by: Miguel Cobian Meraz <mcmmiguel@users.noreply.github.com>
    Co-authored-by: OrlandoLeso <landillo@users.noreply.github.com>

--- App.tsx
+++ App.tsx
@@ -1,10 +1,24 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Text, View, SafeAreaView} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { StackNavigator } from './src/navigation/StackNavigator';
+import firestore from '@react-native-firebase/firestore';
 
 function App(): JSX.Element {
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    const users = await firestore()
+      .collection('test')
+      .doc('GG4SP9YyFlVEEpS81N7J')
+      .get();
+    console.log(users._data.name);
+  };
+
   return (
     <NavigationContainer>
       <StackNavigator />
