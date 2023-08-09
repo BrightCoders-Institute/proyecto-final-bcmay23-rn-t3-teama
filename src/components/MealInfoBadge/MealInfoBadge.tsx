55f6145887929248904f8a28e79c35813047e148
Miguel Cobian <miguelcobian99@gmail.com>
2023-08-08 19:31:17 -0600

    Feat. Types of badge implemented

--- src/components/MealInfoBadge/MealInfoBadge.tsx
+++ src/components/MealInfoBadge/MealInfoBadge.tsx
@@ -4,15 +4,21 @@ import { Image } from 'react-native';
 import { styles } from './styles';
 import { MealInfoBadgeProps } from '../../interfaces/interfaces';
 
+const backgoundType: { [type: string]: string } = {
+    time: '#795DEA',
+    level: '#56ADEC',
+    kcal: '#F3A939',
+};
+
 const MealInfoBadge = ({ type, icon, data }: MealInfoBadgeProps) => {
+    const badgeColor = backgoundType[type];
+    const dataType = type === 'time' ? 'Min' : type === 'level' ? 'Level' : 'Kcal';
+
     return (
-        <View style={[styles.badgeContainer]}>
-            <Image
-                source={icon}
-                style={styles.badgeIcon}
-            />
+        <View style={{ ...styles.badgeContainer, backgroundColor: badgeColor }}>
+            <Image source={icon} style={styles.badgeIcon} />
             <Text style={styles.badgeInfo}>{data}</Text>
-            <Text style={styles.badgeInfo}>{ }</Text>
+            <Text style={styles.badgeInfo}>{dataType}</Text>
         </View>
     );
 };
