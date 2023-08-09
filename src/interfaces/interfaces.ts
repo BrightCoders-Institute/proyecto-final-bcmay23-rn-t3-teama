a92a89a22d7334363f457fcb11a48feb2fc89ad6
Miguel Cobian <miguelcobian99@gmail.com>
2023-08-08 19:11:23 -0600

    Add. MealInfoBadge component

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,31 +1,39 @@
-import { ImageSourcePropType } from "react-native";
+import { ImageSourcePropType } from 'react-native';
+import MealInfoBadge from '../components/MealInfoBadge/MealInfoBadge';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export interface LoadingModalProps {
-    isLoading: boolean;
-    isVisible: boolean;
-    successImageUrl: ImageSourcePropType;
-    errorImageUrl: ImageSourcePropType;
-    title: string;
-    subtitle: string;
-    isSuccessful: boolean;
+  isLoading: boolean;
+  isVisible: boolean;
+  successImageUrl: ImageSourcePropType;
+  errorImageUrl: ImageSourcePropType;
+  title: string;
+  subtitle: string;
+  isSuccessful: boolean;
 }
 
 export interface SubTitleProps {
-    text: string;
-    fontSize?: number;
-    color?: string;
-  }
+  text: string;
+  fontSize?: number;
+  color?: string;
+}
 
-  export interface WellcomeProgressCardProps {
-    title: string;
-    onPress?: () => void;
-    imgSource?: ImageSourcePropType;
-  }
+export interface WellcomeProgressCardProps {
+  title: string;
+  onPress?: () => void;
+  imgSource?: ImageSourcePropType;
+}
 
-  export interface WellnesCardProps {
-    title: string;
-    onPress?: () => void;
-    imgSource?: string;
-    backgroundColor?: string;
-  
-  }
+export interface WellnesCardProps {
+  title: string;
+  onPress?: () => void;
+  imgSource?: string;
+  backgroundColor?: string;
+
+}
+
+export interface MealInfoBadgeProps {
+  type: string;
+  icon: ImageSourcePropType;
+  data: string;
+}
