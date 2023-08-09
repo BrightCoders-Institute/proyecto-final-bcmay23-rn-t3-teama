e85904b43661bfab0b5f55c30623ca63d8e00e44
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-08-09 15:19:57 -0600

    Create nutriotionalChart component file

---
+++ src/components/NutritionalChart/NutritionalChart.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import styles from './styles';
+
+const NutritionalChar = () => {
+  return (
+    <View>
+      <Text>NutritionalChar</Text>
+    </View>
+  );
+};
+
+export default NutritionalChar;
