62d5c743cbfd3e6e8bd1d8a1178e02b8f1b5bbe2
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-08-11 17:48:45 -0600

    Code refactoring

--- src/screens/MyMealDetailsScreen/MyMealDetailsScreen.tsx
+++ src/screens/MyMealDetailsScreen/MyMealDetailsScreen.tsx
@@ -4,13 +4,11 @@ import { Title } from '../../components/Title/Title';
 import { RecipeImg } from '../../components/RecipeImg/RecipeImg';
 import { style } from './styles';
 import { SubTitle } from '../../components/SubTitle/SubTitle';
-import { ButtonPrimary } from '../../components/ButtonPrimary/ButtonPrimary';
 import { RecipeScreen } from '../RecipeScreen/RecipeScreen';
 import { StackScreenProps } from '@react-navigation/stack';
 import MealInfoBadge from '../../components/MealInfoBadge/MealInfoBadge';
 import NutritionalChart from '../../components/NutritionalChart/NutritionalChart';
 import { ButtonSecondary } from '../../components/ButtonSecondary/ButtonSecondary';
-
 
 interface Props extends StackScreenProps<any, any> {}
 
@@ -23,20 +21,22 @@ const MyMealDetailsScreen = ({navigation}: Props) => {
     <View style={style.container}>
         <RecipeImg imgSource={imgRecipe.breakfast}/>
         <View style={style.titleContainer}>
-          <Title text='Fruit Bowl' fontSize={26}/>
-          <SubTitle text='Bowl with fruits, some fruits and more fruits. You can add toppings' fontSize={17} color={'#A9A8A8'}/>
+          <Title text="Fruit Bowl" fontSize={26}/>
+          <SubTitle text="Bowl with fruits, some fruits and more fruits. You can add toppings" fontSize={17} color={'#A9A8A8'}/>
         </View>
         <View style={style.btnRecipe}>
-          <ButtonSecondary title="View Recipe" onPress={RecipeScreen} color={'#795DEA'} borderColor="red" />
+          <ButtonSecondary title="View Recipe" onPress={() => navigation.navigate('Recipe')} color={'#795DEA'} />
         </View>
-
+        <View>
+          <MealInfoBadge minutes={'10-20'} level={'Easy'} kcal={'970'} />
+        </View>
         <View style={style.percentagesTitle}>
-            <Title text='Per Serving' fontSize={20}/>
+            <Title text="Per Serving" fontSize={20}/>
             <NutritionalChart progress={0.4} grams={40}/>
         </View>
 
-        <View style={style.btnRecipe}>
-            <ButtonSecondary title={'Mark as completed'} onPress={RecipeScreen} color={'#58D164'}/>
+        <View style={style.btnCompleted}>
+            <ButtonSecondary title={'Mark as completed'} onPress={() => navigation.navigate('Recipe')} color={'#58D164'}/>
         </View>
 
     </View>
