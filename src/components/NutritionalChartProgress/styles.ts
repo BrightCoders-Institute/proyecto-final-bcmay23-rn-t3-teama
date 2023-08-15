5fc275ab532bac887af83c02434a9b50832cf429
OrlandoLeso <44180680+landillo@users.noreply.github.com>
2023-08-14 23:39:01 -0600

    ShowCalendar and NutritionalChartProgress was created and inplemented in the Report Screen

---
+++ src/components/NutritionalChartProgress/styles.ts
@@ -0,0 +1,55 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        width: 120,
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 10,
+        marginHorizontal: 5,
+        marginVertical: 10,
+    },
+    mainContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#FFFF',
+        alignSelf: 'center',
+        maxWidth: '100%',
+        paddingBottom: -50,
+        marginTop: 10,
+    },
+    progressBar: {
+        transform: [{ rotate: '-90deg' }],
+        height: 18,
+        width: 57,
+        backgroundColor: '#EAEAEA',
+    },
+    infoContainer: {
+        alignContent: 'flex-start',
+        height: 50,
+        width: 80,
+        marginTop: 2,
+        marginRight: 15,
+        marginLeft: -5,
+        // backgroundColor: 'red',
+    },
+    titlenutrition:{
+        paddingTop: 10,
+        margin: 1,
+        fontSize: 15,
+        fontWeight: 'bold',
+        paddingBottom: 6,
+    },
+    percentage: {
+        fontSize: 13,
+        fontWeight: 'bold',
+    },
+
+    gramsText: {
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: '#818181',
+    },
+});
+
--- src/screens/ReportScreen/ReportScreen.tsx
+++ src/screens/ReportScreen/ReportScreen.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { ShowCalendar } from '../../components/ShowCalendar/ShowCalendar';
+import { NutritionalChartProgress } from '../../components/NutritionalChartProgress/NutritionalChartProgress';
 
 export const ReportScreen = () => {
   return (
     <View>
-      <Text>Report Screen</Text>
+      <ShowCalendar />
+      <NutritionalChartProgress 
+        progressCarbs={0.5} 
+        progressProtein={0.3} 
+        progressFat={0.2} 
+        grams={50} 
+        />
     </View>
   );
 };
