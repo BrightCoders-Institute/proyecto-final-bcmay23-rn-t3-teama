b4d96083f110e9316dcc77da30195308f09773e0
Miguel Cobian <miguelcobian99@gmail.com>
2023-08-15 18:11:31 -0600

    Add. TopTab Screens and finished implementation

--- src/navigation/TopTab.tsx
+++ src/navigation/TopTab.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import MySpecialistScreen from '../screens/MySpecialistScreen/MySpecialistScreen';
-import { ProfileScreen } from '../screens/ProfileScreen/ProfileScreen';
-import { useWindowDimensions } from 'react-native';
-import { View, StyleSheet } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import MySpecialistScreen from '../screens/MySpecialistScreen/MySpecialistScreen';
+import MyInfoScreen from '../screens/MyInfoScreen/MyInfoScreen';
 
 const Tab = createMaterialTopTabNavigator();
 
@@ -24,13 +22,13 @@ function TopTab() {
                     fontSize: 18, color: '#FF914D',
                     textTransform: 'capitalize',
                 },
-                tabBarIndicatorStyle: { backgroundColor: '#FF914D' /*maxWidth: '90%', alignSelf: 'center', marginLeft: '7.5%', marginRight: '64%' */ },
+                tabBarIndicatorStyle: { backgroundColor: '#FF914D' },
                 swipeEnabled: true,
                 tabBarGap: 10,
             }}
         >
-            <Tab.Screen name="MySpecialistScreen" options={{ title: 'My Specialist' }} component={ } />
-            <Tab.Screen name="ProfileScreen" options={{ title: 'My Info' }} component={ } />
+            <Tab.Screen name="MySpecialistScreen" options={{ title: 'My Specialist' }} component={MySpecialistScreen} />
+            <Tab.Screen name="MyInfoScreen" options={{ title: 'My Info' }} component={MyInfoScreen} />
         </Tab.Navigator>
     );
 }
