e1b26d28f9f15d0a0829622a92e3f666536c345d
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-08-16 14:11:18 -0600

    Merge changes from main into sprint-3/issue-42#my-specialist-information

---
+++ src/components/RatingStar/RatingStar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+interface RatingStarProps {
+    filled: 'full' | 'half' | 'empty';
+    size: number;
+}
+
+export const RatingStar: React.FC<RatingStarProps> = ({ filled, size }) => {
+
+    const selectStar = () => {
+        const iconMapping: Record<string, string> = {
+            full: 'star',
+            half: 'star-half',
+        };
+        const iconName = iconMapping[filled] || 'star-outline';
+
+        return <Icon name={iconName} size={size} color="gold" />;
+    };
+
+    return (
+        <View style={{ width: size, height: size }}>
+            { selectStar() }
+        </View>
+    );
+};
--- src/screens/MySpecialistScreen/MySpecialistScreen.tsx
+++ src/screens/MySpecialistScreen/MySpecialistScreen.tsx
@@ -1,12 +1,74 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { View, useWindowDimensions, Image, Text, ScrollView } from 'react-native';
+import { styles } from './styles';
+import NutritionistImage from '../../assets/img/nutritionist-profile.png';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { ButtonSecondary } from '../../components/ButtonSecondary/ButtonSecondary';
+import { RatingStar } from '../../components/RatingStar/RatingStar';
 
-const MySpecialistScreen = () => {
+const NUTRITIONIST_INFO = {
+    name: 'Dr. Aimep3 Fischer',
+    major: 'Ph.D. in Nutrition',
+    cityAndCountry: 'Colima, Mx.',
+    biography: 'Harvard-educated nutritionist empowering healthier lives. Personalized meal plans, engaging workshops, and evidence-based guidance for weight management, sports nutrition, and overall well-being. Join our app for a transformative wellness journey today!',
+    rating: '5.0',
+};
+
+export const MySpecialistScreen = () => {
+    const {height} = useWindowDimensions();
+
+    const renderStars = () => {
+        const starElements = [];
+
+        const wholeStars = Math.floor(NUTRITIONIST_INFO.rating);
+        const hasHalfStar = NUTRITIONIST_INFO.rating - wholeStars >= 0.5;
+
+        for (let i = 1; i <= 5; i++) {
+            let filled = 'empty';
+            if (i <= wholeStars) {
+                filled = 'full';
+            } else if (i === wholeStars + 1 && hasHalfStar) {
+                filled = 'half';
+            }
+
+            starElements.push(
+                <View key={i} style={styles.starContainer}>
+                    <RatingStar filled={filled} size={18} />
+                </View>
+            );
+        }
+        return starElements;
+    };
+
     return (
-        <View>
-            <Text>MySpecialistScreen</Text>
-        </View>
+        <ScrollView style={styles.container}>
+            <View style={[{height: height * 0.40}, styles.imageContainer]}>
+                <View style={styles.circle} />
+                <Image
+                    source={NutritionistImage}
+                    style={[styles.nutritionistImage, {height: height * 0.42}]}
+                />
+            </View>
+            <View style={[{height: height * 0.50}, styles.nutritionistInfoContainer]}>
+                <Text style={styles.nutriName}>{NUTRITIONIST_INFO.name}</Text>
+
+                <Text style={[styles.separationText, styles.nutriMajor]}>{NUTRITIONIST_INFO.major}</Text>
+
+                <View style={styles.starRatingContainer}>
+                    <View style={styles.stars}>{renderStars()}</View>
+                    <Text style={styles.nutriRating}>{`${NUTRITIONIST_INFO.rating}`}</Text>
+                </View>
+                <View style={[styles.locationContainer, styles.separationText]}>
+                    <Icon name={'location-sharp'} size={22} color={'#795DEA'}/>
+                    <Text style={styles.nutriCityCountry}>{`${NUTRITIONIST_INFO.cityAndCountry}`}</Text>
+                </View>
+                <View style={{ marginTop: 5 }}>
+                    <Text style={styles.biographyTitle}>Biography</Text>
+                    <Text style={styles.biographyText}>{NUTRITIONIST_INFO.biography}</Text>
+                </View>
+                <ButtonSecondary title="Book Appointment →" color="#795DEA" fontSize={15} onPress={() => console.log('Hola')} />
+            </View>
+        </ScrollView>
     );
 };
 
-export default MySpecialistScreen;
