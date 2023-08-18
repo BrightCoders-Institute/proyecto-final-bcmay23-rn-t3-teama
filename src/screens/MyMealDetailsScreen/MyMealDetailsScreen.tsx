4a0372942ebe17b899cca52b33bd3c889eec3e2d
Montserrat Aguilar <aguilarvalle.montse@gmail.com>
2023-08-18 16:15:27 -0600

    Add modal in MymealDetailsScreen when patient presses mark as completed button

--- src/screens/MyMealDetailsScreen/MyMealDetailsScreen.tsx
+++ src/screens/MyMealDetailsScreen/MyMealDetailsScreen.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View } from 'react-native';
 import { Title } from '../../components/Title/Title';
 import { RecipeImg } from '../../components/RecipeImg/RecipeImg';
@@ -9,16 +9,53 @@ import MealInfoBadge from '../../components/MealInfoBadge/MealInfoBadge';
 import NutritionalChart from '../../components/NutritionalChart/NutritionalChart';
 import { ButtonSecondary } from '../../components/ButtonSecondary/ButtonSecondary';
 import { ScrollView } from 'react-native-gesture-handler';
+import LoadingModal from '../../components/LoadingModal/LoadingModal';
+
+const successCompletedModalImg = require('../../assets/img/successDoctorModal.png');
 
 interface Props extends StackScreenProps<any, any> {}
 
 const MyMealDetailsScreen = ({navigation}: Props) => {
+
+  const [isModalVisible, setModalVisible] = useState(false);
+  const [nutritionalChartData, setNutritionalChartData] = useState(null);
+
+  const handleMarkCompleted = () => {
+    setModalVisible(true);
+
+    const nutritionalData = {
+      progressCarbs: 0.5,
+      progressProtein: 0.3,
+      progressFat: 0.2,
+      gramsCarbs: 50,
+      gramsProtein: 32,
+      gramsFat: 12,
+    };
+    setNutritionalChartData(nutritionalData);
+
+    setTimeout(() => {
+      setModalVisible(false);
+    }, 3000);
+  };
+
 
   const imgRecipe = {
     breakfast: require('../../assets/img/Breakfast.png'),
   };
   return (
     <View style={style.container}>
+
+      <LoadingModal
+        isVisible={isModalVisible}
+        isLoading={false}
+        successImageUrl={successCompletedModalImg}
+        title="Well done"
+        subtitle="Check the report section to know your progress"
+        isSuccessful={true}
+        nutritionalChartData={nutritionalChartData}
+      />
+
+
       <ScrollView>
         <RecipeImg imgSource={imgRecipe.breakfast}/>
         <View style={style.titleContainer}>
@@ -43,7 +80,7 @@ const MyMealDetailsScreen = ({navigation}: Props) => {
             />
         </View>
         <View style={style.btnCompleted}>
-            <ButtonSecondary title={'Mark as completed'} onPress={() => navigation.navigate('Recipe')} color={'#58D164'}/>
+            <ButtonSecondary title={'Mark as completed'} onPress={handleMarkCompleted} color={'#58D164'}/>
         </View>
       </ScrollView>
     </View>
