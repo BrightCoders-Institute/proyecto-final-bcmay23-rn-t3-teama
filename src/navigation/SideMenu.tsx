3865d7badd5bba9a7617c9f2b3a83d6e3e5a2afc
Miguel Cobian <miguelcobian99@gmail.com>
2023-09-08 19:46:03 -0600

    Drawer design updated

--- src/navigation/SideMenu.tsx
+++ src/navigation/SideMenu.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { DrawerContentComponentProps, DrawerContentScrollView, createDrawerNavigator } from '@react-navigation/drawer';
 import { Image, TouchableOpacity, View, useWindowDimensions, Text, StyleSheet } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import { StackNavigator } from './StackNavigator';
+import { AppContext } from '../context/AppContext';
+import auth from '@react-native-firebase/auth';
 
 const Drawer = createDrawerNavigator();
 
 const SideMenu = () => {
     const { width } = useWindowDimensions();
+    const { appState: { isLoggedIn } } = useContext(AppContext);
 
     return (
         <Drawer.Navigator
@@ -18,51 +21,102 @@ const SideMenu = () => {
                 }}
             drawerContent={(props) => <MenuInterno {...props} />}
         >
-            <Drawer.Screen name="Tabs" component={StackNavigator} />
+            {/* {isLoggedIn ? } */}
+            <Drawer.Screen name="StackNavigator" component={StackNavigator} />
         </Drawer.Navigator>
     );
 };
 
 const MenuInterno = ({ navigation }: DrawerContentComponentProps) => {
+
+    const { appState: { userData }, logOut } = useContext(AppContext);
+
+    const logout = () => {
+        auth()
+            .signOut()
+            .then(() => {
+                logOut();
+                console.log('User signed out!');
+            });
+    };
+
     return (
         <DrawerContentScrollView>
 
             {/* Parte del avatar */}
-            <View style={styles.avatarContainer}>
+            < View style={styles.avatarContainer} >
                 <Image
                     source={{
                         uri: 'https://www.seekpng.com/png/small/143-1435868_headshot-silhouette-person-placeholder.png',
                     }}
                     style={styles.avatar}
                 />
-            </View>
+                <Text style={styles.userName}>{userData.name}</Text>
+            </ View>
 
 
             {/* Opciones de Menú*/}
-            <View style={styles.menuContainer}>
+            < View style={styles.menuContainer} >
                 <TouchableOpacity
                     style={{
                         ...styles.menuButton,
                         flexDirection: 'row',
                     }}
-                    onPress={() => navigation.navigate('StackNavigator')}
+                    onPress={() => {
+                        navigation.navigate('StackNavigator', {
+                            screen: 'BottomTab',
+                            params: {
+                                screen: 'HomeScreen',
+                            },
+                        });
+                    }}
                 >
-                    <Icon name="home" size={25} color="grey" />
+                    <Icon name="home" size={25} color={colors.primary} />
                     <Text style={styles.menuText}> Home</Text>
                 </TouchableOpacity>
+
                 <TouchableOpacity
                     style={{
                         ...styles.menuButton,
                         flexDirection: 'row',
                     }}
-                    onPress={() => navigation.navigate('SettingsScreen')}
+                    onPress={() => navigation.navigate('Book Appointment')}
                 >
-                    <Icon name="cog-outline" size={25} color="grey" />
-                    <Text style={styles.menuText}> Ajustes</Text>
+                    <Icon name="calendar" size={25} color={colors.primary} />
+                    <Text style={styles.menuText}> Book an appointment</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                    style={{
+                        ...styles.menuButton,
+                        flexDirection: 'row',
+                    }}
+                    onPress={() => {
+                        navigation.navigate('StackNavigator', {
+                            screen: 'BottomTab',
+                            params: {
+                                screen: 'TopTab',
+                                params: {
+                                    screen: 'MyInfoScreen',
+                                },
+                            },
+                        });
+                    }}
+                >
+                    <Icon name="person" size={25} color={colors.primary} />
+                    <Text style={styles.menuText}> Profile</Text>
+                </TouchableOpacity>
+
+            </ View>
+
+            <View style={{ marginTop: '75%' }} >
+                <TouchableOpacity
+                    onPress={logout}
+                >
+                    <Text style={styles.logoutButton}>Log Out</Text>
                 </TouchableOpacity>
             </View>
-
-        </DrawerContentScrollView>
+        </DrawerContentScrollView >
     );
 };
 
@@ -70,7 +124,7 @@ export default SideMenu;
 
 
 export const colors = {
-    primary: '#5856d6',
+    primary: '#795DEA',
 
 };
 
@@ -106,15 +160,33 @@ export const styles = StyleSheet.create({
         height: 150,
         borderRadius: 100,
     },
+    userName: {
+        color: 'black',
+        marginTop: 10,
+    },
     menuContainer: {
         marginVertical: 30,
         marginHorizontal: 30,
+        justifyContent: 'space-between',
+        // backgroundColor: 'red',
     },
     menuButton: {
         marginVertical: 10,
     },
     menuText: {
         fontSize: 20,
-
+        color: 'black',
+    },
+    logoutButton: {
+        flex: 1,
+        backgroundColor: 'red',
+        marginTop: 20,
+        marginHorizontal: 20,
+        fontSize: 15,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: 'white',
+        padding: 10,
+        borderRadius: 15,
     },
 });
