544e921164f78ccb82a745582b2cc51cbc1cd996
AlbertoG22 <sergioalberto_garcia@ucol.mx>
2023-09-12 17:18:04 -0600

    Recover last changes from PR 86

--- src/components/ButtonSecondary/ButtonSecondary.tsx
+++ src/components/ButtonSecondary/ButtonSecondary.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import {Text, TouchableOpacity, ViewStyle} from 'react-native';
-import {style} from './styles';
-import {ButtonSecondaryProps} from '../../interfaces/interfaces';
+import { Text, TouchableOpacity, ViewStyle } from 'react-native';
+import { style } from './styles';
+import { ButtonSecondaryProps } from '../../interfaces/interfaces';
 
-export const ButtonSecondary = ({title, onPress, color, fontSize = 20}: ButtonSecondaryProps) => {
+export const ButtonSecondary = ({ title, onPress, color, fontSize = 20, isDisabled }: ButtonSecondaryProps) => {
   const buttonStyle: ViewStyle = {
-    backgroundColor: color || '#007bff',
+    backgroundColor: isDisabled ? '#A0A0A0' : color || '#007bff',
   };
 
   return (
     <TouchableOpacity
+      disabled={isDisabled}
       style={[style.buttonContainer, buttonStyle]}
       onPress={onPress}>
-      <Text style={[style.buttonText, {fontSize: fontSize}]}>{title}</Text>
+      <Text style={[style.buttonText, { fontSize: fontSize }]}>{title}</Text>
     </TouchableOpacity>
   );
 };
